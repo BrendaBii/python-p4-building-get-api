5612e9b2edd4422f7de5fe9c03b8d6b41ae40de1
BrendaBii <bii.brenda.bcb@gmail.com>
2023-09-21 17:39:34 +0300

    Completed assignment.

--- server/models.py
+++ server/models.py
@@ -1,14 +1,63 @@
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData
+
+# metadata = MetaData(naming_convention={
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# })
+
+# db = SQLAlchemy(metadata=metadata)
+
+# class Game(db.Model):
+#     __tablename__ = 'games'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String, unique=True)
+#     genre = db.Column(db.String)
+#     platform = db.Column(db.String)
+#     price = db.Column(db.Integer)
+#     created_at = db.Column(db.DateTime, server_default=db.func.now())
+#     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+#     reviews = db.relationship('Review', backref='game')
+
+#     def __repr__(self):
+#         return f'<Game {self.title} for {self.platform}>'
+
+# class Review(db.Model):
+#     __tablename__ = 'reviews'
+    
+#     id = db.Column(db.Integer, primary_key=True)
+#     score = db.Column(db.Integer)
+#     comment = db.Column(db.String)
+#     created_at = db.Column(db.DateTime, server_default=db.func.now())
+#     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+#     game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+#     def __repr__(self):
+#         return f'<Review ({self.id}) of {self.game}: {self.score}/10>'
+
+# class User(db.Model):
+#     __tablename__ = 'users'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+    
+#     created_at = db.Column(db.DateTime, server_default=db.func.now())
+#     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+#     reviews = db.relationship('Review', backref='user')
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-class Game(db.Model):
+class Game(db.Model, SerializerMixin):
     __tablename__ = 'games'
+
+    serialize_rules = ('-reviews.game',)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True)
@@ -23,9 +72,11 @@ class Game(db.Model):
     def __repr__(self):
         return f'<Game {self.title} for {self.platform}>'
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
-    
+
+    serialize_rules = ('-game.reviews', '-user.reviews',)
+
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
     comment = db.Column(db.String)
@@ -38,12 +89,14 @@ class Review(db.Model):
     def __repr__(self):
         return f'<Review ({self.id}) of {self.game}: {self.score}/10>'
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+
+    serialize_rules = ('-reviews.user',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    
+
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
